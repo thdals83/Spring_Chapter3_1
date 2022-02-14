b91bd8798e3ae7d5079a7de01b751cfe2b6c4b1b
thdals83 <thdals83@naver.com>
2022-02-14 17:17:59 +0900

    start

---
+++ src/main/java/com/chapter3_1/spring_chapter3_1/SpringChapter31Application.java
@@ -0,0 +1,14 @@
+package com.chapter3_1.spring_chapter3_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringChapter31Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringChapter31Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/chapter3_1/spring_chapter3_1/SpringChapter31ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chapter3_1.spring_chapter3_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChapter31ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
